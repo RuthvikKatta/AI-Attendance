318ca6caf0e940bede1956cb697597531b70cad5
RUTHVIK <katta.ruthvik7@gmail.com>
2023-01-20 18:44:22 +0530

    initial commit

---
+++ JavaScript/Functions.js
@@ -0,0 +1,119 @@
+// Function Display()
+
+var str;
+
+function display() {;
+    str = ''
+    var str1 = '';
+    var str2 = '';
+    var day = '';
+    var count1 = 0;
+    var count2 = 0;
+    var present = document.getElementsByClassName('left');
+    for (let one of present) {
+        if (one.getAttribute("is-present") === 'true') {
+            if (one.id.substring(0, 3) === '209') {
+                str1 = str1 + one.id.substring(8, 10) + ', ';
+                count1 += 1;
+            }
+            else {
+                str1 = str1 + 'LE' + one.id.substring(8, 10) + ', ';
+                count1 += 1;
+            }
+        }
+        else if (one.getAttribute("is-present") === 'false') {
+            if (one.id.substring(0, 3) === '209') {
+                str2 = str2 + one.id.substring(8, 10) + ', ';
+                count2 += 1;
+            }
+            else {
+                str2 = str2 + 'LE' + one.id.substring(8, 10) + ', ';
+                count2 += 1;
+            }
+        }
+    }
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let currentDate = new Date().toLocaleDateString("en-US", options);
+    str += currentDate + '\n';
+    if (count1 == 1)
+        str += "Presentees (" + count1 + "  Member)\n";
+    else
+        str += "Presentees (" + count1 + "  Members)\n";
+    str += str1 + '\n';
+    if (count2 == 1)
+        str += "Absentees (" + count2 + "  Member)\n";
+    else
+        str += "Absentees (" + count2 + "  Members)\n";
+    str += str2 + '\n';
+    switch (new Date().getHours()) {
+        case 9:
+            day = "In First Hour.";
+            break;
+        case 10:
+            day = "In Second Hour.";
+            break;
+        case 11:
+            day = "In Third Hour.";
+            break;
+        case 12:
+            day = "In Fourth Hour.";
+            break;
+        case 14:
+            day = "In Fifth Hour.";
+            break;
+        case 15:
+            day = "In Sixth Hour.";
+            break;
+    }
+    str += day;
+    document.getElementById('display').value = str;
+
+    //Display Border colour 
+    if (count1 > count2)
+        document.getElementById('display').style.borderColor = 'lightgreen';
+    else if (count1 < count2)
+        document.getElementById('display').style.borderColor = '#ff5c5c';
+    else if (count1 == 0)
+        document.getElementById('display').style.borderColor = 'rgb(165, 165, 165)';
+    else
+        document.getElementById('display').style.borderColor = '#f6f672'; 
+}
+
+// Whatsapp Message sender Function
+
+document.querySelector(".Whatsapp").addEventListener('click', function () {
+    var message = encodeURIComponent(str);
+    window.open(`whatsapp://send?text=${message}`);
+})
+
+
+
+// Copy Function
+
+let copy = document.querySelector("#copy");
+copy.addEventListener('click', function () {
+    let input = document.querySelector("#display").select();
+    document.execCommand("copy");
+    copy.classList.add("active");
+    window.getSelection().removeAllRanges();
+    setTimeout(function () {
+        copy.classList.remove("active");
+    }, 2500);
+})
+
+// Reset Function
+
+document.getElementById("reset").addEventListener('click', function () {
+    var present = document.getElementsByClassName('left');
+    for (let one of present) {
+        var leftID = one.id;
+        var RightID = leftID.substring(0, 10) + 'R';
+        one.setAttribute("is-present", 'none');
+        document.getElementById(leftID).classList = "left";
+        document.getElementById(RightID).classList = "right";
+    }
+    document.getElementById('display').value = '';
+    document.getElementById('display').style.borderColor = 'rgb(165, 165, 165)';
+    // document.getElementById('Details').textContent = 'Details : ';
+    // document.getElementById('subject').value = '';
+});
