24f52d6fef3c66051923acf320e4e1974b6a5e4a
RUTHVIK <katta.ruthvik7@gmail.com>
2023-09-04 22:09:35 +0530

    Display with Name Button

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -5,123 +5,112 @@ const subject = document.getElementById('subject');
 const popup = document.getElementById('popup');
 const swh = document.getElementById('switch');
 const copy = document.querySelector("#copy");
+const nameButton = document.querySelector('.toggle-names-switch-button');
 let section = select.value;
 let AI = true;
-let str = '';
-let count1 = 0;
-let count2 = 0;
-let str1 = '';
-let str2 = '';
+let str; // declared as global for whatsapp API access
 
 generate();
 
 select.addEventListener('change', function () {
-    section = select.value;
-    document.getElementById("section").innerText = select.options[select.selectedIndex].text;
-    document.getElementById('display').value = '';
-    document.getElementById('display').style.borderColor = 'rgb(165, 165, 165)';
-    subject.value = '';
-    document.getElementById('switch').checked = false;
-    document.getElementById('popup').style.display = 'none';
-    AI = section === 'AI';
-    if (section === 'PBT' || section === 'SBT') {
-        subject.value = 'Training and Placement';
-    }
-    generate();
+	section = select.value;
+	document.getElementById("section").innerText = select.options[select.selectedIndex].text;
+	document.getElementById('display').value = '';
+	document.getElementById('display').style.borderColor = 'rgb(165, 165, 165)';
+	subject.value = '';
+	document.getElementById('switch').checked = false;
+	document.getElementById('popup').style.display = 'none';
+	AI = section === 'AI';
+	if (section === 'PBT' || section === 'SBT') {
+		subject.value = 'Training and Placement';
+	}
+	generate();
 })
 
-subject.addEventListener("change",function(){
-    if (subject.value === 'other') 
-        popup.style.display = 'block';
+subject.addEventListener("change", function () {
+	if (subject.value === 'other')
+		popup.style.display = 'block';
 	else {
 		popup.style.display = 'none';
 		document.getElementById('other').value = '';
 	}
-    display();
+	display();
 })
 
 swh.addEventListener("change", function () {
-    generate();
+	generate();
 });
 
 function generate() {
-    var parent = document.querySelector(".data");
-    let content = "";
-    parent.innerHTML = "";
-    if (swh.checked === false) {
-        for (let one of data) {
-            if (AI || one.Section == section) {
-                content += `
-                <div class="tab">
-                    <p><span>Roll Number :</span>${one.Id}</p>
-                    <p><span>Name :</span>${one.Name}</p>
-                    <div class="Attendance green" id=${one.Id}  is-present="true" ><p>Present</p></div> 
+	var parent = document.querySelector(".data");
+	let content = "";
+	parent.innerHTML = "";
+	for (let one of data) {
+		if (AI || one.Section == section) {
+			const attendanceClass = swh.checked ? "red" : "green";
+			const attendanceText = swh.checked ? "Absent" : "Present";
+
+			content += `
+            <div class="tab">
+                <p><span>Roll Number :</span>${one.Id}</p>
+                <p><span>Name :</span>${one.Name}</p>
+                <div class="Attendance ${attendanceClass}" id="${one.Id}" name="${one.Name}" is-present="${!swh.checked}">
+                    <p>${attendanceText}</p>
                 </div>
-                `
-            }
-        }
-    } else {
-        for (let one of data) {
-            if (AI || one.Section == section) {
-                content += `
-                <div class="tab">
-                    <p><span>Roll Number :</span>${one.Id}</p>
-                    <p><span>Name :</span>${one.Name}</p>
-                    <div class="Attendance red" id=${one.Id}  is-present="false" ><p>Absent</p></div> 
-                </div>
-                `
-            }
-        }
-    }
-    parent.innerHTML = content;
-    display();
-    const tabs = document.querySelectorAll(".Attendance");
-    tabs.forEach((tab) => {
-        tab.addEventListener("click", function () {
-            const isPresent = tab.getAttribute("is-present") === "true";
-            const colorClass = isPresent ? "red" : "green";
-            const text = isPresent ? "Absent" : "Present";
-            tab.classList.toggle("green", !isPresent);
-            tab.classList.toggle("red", isPresent);
-            tab.setAttribute("is-present", String(!isPresent));
-            tab.innerHTML = `<p>${text}</p>`;
-            display();
-        });
-    });
+            </div>
+        `;
+		}
+	}
+	parent.innerHTML = content;
+
+	display();
+	const tabs = document.querySelectorAll(".Attendance");
+	tabs.forEach((tab) => {
+		tab.addEventListener("click", function () {
+			const isPresent = tab.getAttribute("is-present") === "true";
+			const text = isPresent ? "Absent" : "Present";
+			tab.classList.toggle("green", !isPresent);
+			tab.classList.toggle("red", isPresent);
+			tab.setAttribute("is-present", String(!isPresent));
+			tab.innerHTML = `<p>${text}</p>`;
+			display();
+		});
+	});
 }
+
+nameButton.addEventListener('change', () => {
+	display();
+})
 
 // Function Display()
 
 function display() {
 	str = '';
-	str1 = '';
-	str2 = '';
-	count1 = 0;
-	count2 = 0;
-
+	let str1 = '';
+	let str2 = '';
+	let count1 = 0;
+	let count2 = 0;
 	let subject;
 
 	const present = document.querySelectorAll('.Attendance');
 
 	for (let one of present) {
-		if (one.getAttribute('is-present') === 'true') {
-			if (one.id.substring(0, 3) === '209') {
-				str1 += one.id.substring(8, 10) + ', ';
-				count1++;
-			} else {
-				str1 += 'LE' + one.id.substring(8, 10) + ', ';
-				count1++;
-			}
-		} else if (one.getAttribute('is-present') === 'false') {
-			if (one.id.substring(0, 3) === '209') {
-				str2 += one.id.substring(8, 10) + ', ';
-				count2++;
-			} else {
-				str2 += 'LE' + one.id.substring(8, 10) + ', ';
-				count2++;
-			}
+		const isPresent = one.getAttribute('is-present') === 'true';
+		const prefix = one.id.substring(0, 3) === '209' ? '' : 'LE';
+		const name = one.getAttribute('name');
+		const haveName = nameButton.checked;
+		const entry = `${prefix}${one.id.substring(8, 10)}${haveName ? ` - ${name}` : ''}`;
+	
+		if (isPresent) {
+			str1 += `${entry}, `;
+			count1++;
+		} else {
+			str2 += `${entry}, `;
+			count2++;
 		}
 	}
+	
+	
 
 	str1 = str1.slice(0, -2);
 	str2 = str2.slice(0, -2);
@@ -129,54 +118,45 @@ function display() {
 	const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 	const currentDate = new Date().toLocaleDateString('en-US', options);
 
-	str += document.getElementById('section').innerText + ' ( IV year ) Attendance \n';
+	str += `${document.getElementById('section').innerText} ( IV year ) Attendance \n`;
 
-	if (count1 == 1) {
-		str += 'Presentees (' + count1 + '  Member)\n';
-	} else {
-		str += 'Presentees (' + count1 + '  Members)\n';
-	}
+    str += `Presentees (${count1} Member${count1 === 1 ? '' : 's'})\n`;
+    str += `${str1}\n\n`;
 
-	str += str1 + '\n';
+    str += `Absentees (${count2} Member${count2 === 1 ? '' : 's'})\n`;
+    str += `${str2}\n`;
 
-	if (count2 == 1) {
-		str += 'Absentees (' + count2 + '  Member)\n';
-	} else {
-		str += 'Absentees (' + count2 + '  Members)\n';
-	}
+    const select = document.getElementById('subject');
+    subject = select.value;
 
-	str += str2 + '\n';
+    select.addEventListener('change', ( ) => {
+        subject = select.value;
+    });
 
-	const select = document.getElementById('subject');
-	subject = select.value;
+    if (subject === 'other') {
+        subject = document.getElementById('other').value;
+    }
 
-	select.addEventListener('change', (event) => {
-		subject = select.value;
-	});
+    str += `in ${subject} on ${currentDate}\n`;
 
-	if (subject == 'other') {
-		subject = document.getElementById('other').value;
-	}
+    document.getElementById('display').value = str;
 
-	str += 'in ' + subject + ' on ' + currentDate + '\n';
-
-	document.getElementById('display').value = str;
-
-	//Display border color 
-	switch (true) {
-		case (count1 > count2):
-			document.getElementById('display').style.borderColor = 'lightgreen';
-			break;
-		case (count1 < count2):
-			document.getElementById('display').style.borderColor = '#ff5c5c';
-			break;
-		case (count1 == 0):
-			document.getElementById('display').style.borderColor = 'rgb(165, 165, 165)';
-			break;
-		default:
-			document.getElementById('display').style.borderColor = '#f6f672';
-			break;
-	}
+    // Display border color 
+    const displayElement = document.getElementById('display');
+    switch (true) {
+        case (count1 > count2):
+            displayElement.style.borderColor = 'lightgreen';
+            break;
+        case (count1 < count2):
+            displayElement.style.borderColor = '#ff5c5c';
+            break;
+        case (count1 === 0):
+            displayElement.style.borderColor = 'rgb(165, 165, 165)';
+            break;
+        default:
+            displayElement.style.borderColor = '#f6f672';
+            break;
+    }
 }
 
 // Whatsapp Message sender Function
@@ -206,7 +186,7 @@ document.getElementById("reset").addEventListener('click', function () {
 	const present = document.querySelectorAll('.Attendance');
 	const switchStatus = document.getElementById('switch').checked;
 	for (let one of present) {
-		if(switchStatus ==  false){
+		if (switchStatus == false) {
 			one.setAttribute("is-present", 'true');
 			one.classList = "Attendance green";
 			one.lastChild.innerHTML = "Present";
@@ -220,5 +200,6 @@ document.getElementById("reset").addEventListener('click', function () {
 	document.getElementById('display').style.borderColor = 'rgb(165, 165, 165)';
 	document.getElementById('switch').checked = switchStatus;
 	document.getElementById('popup').style.display = 'none';
+	nameButton.checked = false;
 	display();
 });
